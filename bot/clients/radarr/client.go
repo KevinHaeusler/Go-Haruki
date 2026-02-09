809782f8fae542c2c565ce018fc38bdef35d91c4
KevinHaeusler <kevin@kh.dev>
2026-02-09 19:34:29 +0100

    Implement Sonarr/Radarr http client

---
+++ bot/clients/radarr/client.go
@@ -0,0 +1,42 @@
+package radarr
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/KevinHaeusler/go-haruki/bot/httpx"
+)
+
+type Client struct {
+	BaseURL string
+	APIKey  string
+	HTTP    *httpx.Client
+}
+
+func New(baseURL, apiKey string, http *httpx.Client) *Client {
+	return &Client{
+		BaseURL: strings.TrimRight(baseURL, "/"),
+		APIKey:  apiKey,
+		HTTP:    http,
+	}
+}
+
+func (c *Client) headers() map[string]string {
+	return map[string]string{
+		"X-Api-Key": c.APIKey,
+	}
+}
+
+type SystemStatus struct {
+	Version string `json:"version"`
+}
+
+func (c *Client) GetSystemStatus(ctx context.Context) (*SystemStatus, error) {
+	var out SystemStatus
+	url := fmt.Sprintf("%s/api/v3/system/status", c.BaseURL)
+	if err := c.HTTP.DoJSON(ctx, "GET", url, c.headers(), nil, &out); err != nil {
+		return nil, err
+	}
+	return &out, nil
+}
